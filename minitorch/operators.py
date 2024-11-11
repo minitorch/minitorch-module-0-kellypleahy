c4a2f94d1fa4199498b0cb38be6f7ec582cb0c1b
Kelly Leahy <kelly.p.leahy@gmail.com>
2024-11-10 21:11:22 -0800

    module 0 tests passing

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -31,9 +31,60 @@ from typing import Callable, Iterable
 # For is_close:
 # $f(x) = |x - y| < 1e-2$
 
-
 # TODO: Implement for Task 0.1.
 
+def mul(a, b):
+    return a * b
+
+def id(a):
+    return a
+
+def add(a, b):
+    return a + b
+
+def neg(a):
+    return -a
+
+def lt(a, b):
+    return a < b
+
+def eq(a, b):
+    return a == b
+
+def max(a, b):
+    return b if lt(a, b) else a
+
+def is_close(a, b):
+    return lt(add(a, neg(b)), 1e-2) if lt(b, a) else lt(add(b, neg(a)), 1e-2)
+
+def sigmoid(a):
+    if lt(a, 0):
+        ea = exp(a)
+        return ea * inv(add(1, ea))
+    return inv(add(1, exp(-a)))
+
+def relu(a):
+    return max(0, a)
+
+def log(a):
+    return math.log(a)
+
+def exp(a):
+    return math.exp(a)
+
+def inv(a):
+    return 1 / a
+
+def log_back(a, b):
+    return mul(b, inv(a))
+
+def inv_back(a, b):
+    return mul(neg(inv(mul(a, a))), b)
+
+def relu_back(a, b):
+    if lt(a, 0):
+        return 0
+    return b
 
 # ## Task 0.3
 
@@ -52,3 +103,34 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.3.
+def map(f, a):
+    for v in a:
+        yield f(v)
+
+def zipWith(a, b):
+    a_iter = a.__iter__()
+    b_iter = b.__iter__()
+    while True:
+        try:
+            a_v = a_iter.__next__()
+            b_v = b_iter.__next__()
+            yield a_v, b_v
+        except StopIteration:
+            return
+
+def reduce(f, l, v):
+    for lv in l:
+        v = f(v, lv)
+    return v
+
+def negList(a):
+    return map(lambda x: -x, a)
+
+def addLists(a, b):
+    return [(av + bv) for (av, bv) in zipWith(a, b)]
+
+def sum(a):
+    return reduce(add, a, 0.0)
+
+def prod(a):
+    return reduce(mul, a, 1.0)
